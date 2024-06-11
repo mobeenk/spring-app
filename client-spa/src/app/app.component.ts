645c0c0b53a3fbc9cfe99e915ea51b71419f77ae
Moubien <mobenn-k@hotmail.com>
2024-06-11 23:26:21 +0300

    adjust, english, arabic layour

--- client-spa/src/app/app.component.ts
+++ client-spa/src/app/app.component.ts
@@ -1,22 +1,26 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { HttpClientModule } from '@angular/common/http';
+import { I18nService } from './services/i18n.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements AfterViewInit {
+  constructor(private i18nService: I18nService, private cdr: ChangeDetectorRef) {}
   title = 'client-spa';
   ngAfterViewInit() {
-    const preloader = document.getElementById('preloader');
-    if (preloader) {
-      preloader.style.display = 'none';
-    }
+ 
+  }
+  switchLanguage(language: string) {
+    this.i18nService.switchLanguage(language);
+    this.cdr.detectChanges(); // Trigger change detection
   }
 }
---
+++ client-spa/src/app/services/i18n.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class I18nService {
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['en', 'ar']);
+    const browserLang = this.translate.getBrowserLang();
+    const defaultLang = 'en';
+    this.translate.use(browserLang && browserLang.match(/en|ar/) ? browserLang : defaultLang);
+    this.setDirection(this.translate.currentLang);
+    // this.translate.use(browserLang.match(/en|ar/) ? browserLang : 'en');
+  }
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+    this.setDirection(lang);
+  }
+
+  private setDirection(lang: string) {
+    const direction = lang === 'ar' ? 'rtl' : 'ltr';
+    document.documentElement.dir = direction;
+    const resumeContent = document.querySelector('.resume-content') as HTMLElement;
+    if (resumeContent) {
+      if (direction === 'rtl') {
+        resumeContent.style.marginRight = '260px';
+        resumeContent.style.marginLeft = '0px';
+      } else {
+        resumeContent.style.marginLeft = '260px'; // or whatever your original value is
+        resumeContent.style.marginRight = '0px';
+      }
+    }
+  }
+
+
+}
