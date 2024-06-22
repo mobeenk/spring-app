527fc3a5f54eedc0bd65f96d5b86384c6b2c67b6
Moubien <mobenn-k@hotmail.com>
2024-06-22 20:09:43 +0300

    fix icon and links

--- client-spa/src/environments/environment.development.ts
+++ client-spa/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:8080/'
+    baseUrl: 'http://localhost:8080/',
+    adanUrl: 'https://api.aladhan.com/v1/timingsByCity'
 };
--- client-spa/src/environments/environment.ts
+++ client-spa/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://moubien-kayali.com/api/'
+    baseUrl: 'https://moubien-kayali.com/api/',
+    adanUrl: 'https://api.aladhan.com/v1/timingsByCity'
 };
